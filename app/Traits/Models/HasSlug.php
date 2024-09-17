95f0e80a79a532110ff425b1e1121aa318aabfed
Denis Shevchenko <webmaster@gartal.cz>
2024-09-17 18:17:54 +0400

    Improve slug generation

---
+++ app/Traits/Models/HasSlug.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait HasSlug
+{
+    protected static function bootHasSlug()
+    {
+        static::creating(function (Model $model) {
+            if ($model->slug === null) {
+                $slug = $originSlug = str($model->{self::slugFrom()})->slug();
+
+                $version = 1;
+                while (static::query()->where('slug', $slug)->exists()) {
+                    $slug = str($originSlug)->append('-' . $version)->slug();
+                    $version++;
+                }
+
+                $model->slug = $slug;
+            }
+        });
+    }
+
+    public static function slugFrom(): string
+    {
+        return 'title';
+    }
+}
--- database/migrations/2024_09_17_115547_create_categories_table.php
+++ database/migrations/2024_09_17_115547_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         if (app()->isLocal()) {
             Schema::create('categories', function (Blueprint $table) {
                 $table->id();
-                $table->string('slug');
+                $table->string('slug')->unique();
                 $table->string('title');
                 $table->timestamps();
             });
