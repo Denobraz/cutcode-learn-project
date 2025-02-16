fcd7f7f4345f4dcaf3f630ba089787b255f7b346
Denis Shevchenko <webmaster@gartal.cz>
2025-02-16 15:35:00 +0400

    Add slug to shop models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use App\Support\Traits\Models\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Support\Traits\Models\HasSlug;
 
 class Category extends Model
 {
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use App\Support\Traits\Models\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Support\Traits\Models\HasSlug;
 
 class Product extends Model
 {
